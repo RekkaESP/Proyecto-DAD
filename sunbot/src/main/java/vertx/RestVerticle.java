5646e4350a9d3cf61671e558190c006a2629acd0
ACCastill0 <61787181+ACCastill0@users.noreply.github.com>
2020-04-06 12:20:42 +0200

    Update RestVerticle.java

--- sunbot/src/main/java/vertx/RestVerticle.java
+++ sunbot/src/main/java/vertx/RestVerticle.java
@@ -25,31 +25,68 @@ public class RestVerticle extends AbstractVerticle {
 		Router router = Router.router(vertx);
 		vertx.createHttpServer().requestHandler(router::accept).listen(8090, result->{});
 		router.route("/api/sensors*").handler(BodyHandler.create());
+		/*Humidity*/
 		router.get("/api/humidity").handler(this::getAllHumidities);
-		router.get("/api/temperature").handler(this::getAllTemperatures);
-		router.get("/api/luminosity").handler(this::getAllLuminosities);
 		router.put("/api/humidity").handler(this::addOneHumidity);
-		router.put("/api/temperature").handler(this::addOneTemperature);
-		router.put("/api/luminosity").handler(this::addOneLuminosity);
 		router.delete("/api/humidity").handler(this::deleteOneHumidity);
-		router.delete("/api/temperature").handler(this::deleteOneTemperature);
-		router.delete("/api/luminosity").handler(this::deleteOneLuminosity);
 		router.post("/api/humidity").handler(this::postOneHumidity);
+		/*Temperature*/
+		router.get("/api/temperature").handler(this::getAllTemperatures);
+		router.put("/api/temperature").handler(this::addOneTemperature);
+		router.delete("/api/temperature").handler(this::deleteOneTemperature);
 		router.post("/api/temperature").handler(this::postOneTemperature);
+		/*Luminosity*/
+		router.get("/api/luminosity").handler(this::getAllLuminosities);		
+		router.put("/api/luminosity").handler(this::addOneLuminosity);		
+		router.delete("/api/luminosity").handler(this::deleteOneLuminosity);		
 		router.post("/api/luminosity").handler(this::postOneLuminosity);
 	}
-	private void getAll(RoutingContext routingContext) {
+	////////////
+	/*Humidity*/
+	////////////
+	private void getAllHumidities(RoutingContext routingContext) {
 		
 	}
-	private void addOne(RoutingContext routingContext) {
+	private void addOneHumidity(RoutingContext routingContext) {
 		
 	}
-	private void deleteOne(RoutingContext routingContext) {
+	private void deleteOneHumidity(RoutingContext routingContext) {
 		
 	}
-	private void postOne(RoutingContext routingContext) {
+	private void postOneHumidity(RoutingContext routingContext) {
 		
 	}
+	///////////////
+	/*Temperature*/
+	///////////////
+	private void getAllTemperatures(RoutingContext routingContext) {
+		
+	}
+	private void addOneTemperature(RoutingContext routingContext) {
+		
+	}
+	private void deleteOneTemperature(RoutingContext routingContext) {
+		
+	}
+	private void postOneTemperature(RoutingContext routingContext) {
+		
+	}
+	//////////////
+	/*Luminosity*/
+	//////////////
+	private void getAllLuminosities(RoutingContext routingContext) {
+		
+	}
+	private void addOneLuminosity(RoutingContext routingContext) {
+		
+	}
+	private void deleteOneLuminosity(RoutingContext routingContext) {
+		
+	}
+	private void postOneLuminosity(RoutingContext routingContext) {
+		
+	}
+	
 	public void stop(Future<Void> stopFuture) throws Exception{
 		super.stop(stopFuture);
 	}
