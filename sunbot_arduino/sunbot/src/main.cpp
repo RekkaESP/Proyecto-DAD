20b382849f0db35529cb88f4ed6c51f619089e6d
ACCastill0 <61787181+ACCastill0@users.noreply.github.com>
2020-05-28 10:51:40 +0200

    Cliente MQTT arduino

--- sunbot_arduino/sunbot/src/main.cpp
+++ sunbot_arduino/sunbot/src/main.cpp
@@ -1,18 +1,35 @@
+
 #include <Arduino.h>
 #include "ArduinoJson.h"
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <SoftwareSerial.h>
+#include <PubSubClient.h>
 
 char responseBuffer[300];
 WiFiClient client;
 
-String SSID = "";
-String PASS = "";
+String SSID = "FTTH_Palacios_ilCqwk";
+String PASS = "3X76dhNs";
 
-String SERVER_IP = "";
+String SERVER_IP = "192.168.100.46";
 int SERVER_PORT = 8090;
 
+///////-MQTT-//////////
+///////////////////////
+PubSubClient MQTTclient(client);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+//IPAddress mqtt_server(127, 0, 0, 1);
+const char* mqtt_server = "192.168.100.46";
+const char* mqtt_username = "mqttbroker";
+const char* mqtt_password = "mqttbrokerpass";
+///////////////////////
+///////////////////////
+void callback(char* topic, byte* payload, unsigned int length);
+void reconnect();
 void sendGetSensor(int);
 void sendGetMotor(int);
 void sendPostSensor(int,float,float,long);
@@ -20,9 +37,9 @@ void sendPostMotor(int,float,long);
 
 void setup() {
   Serial.begin(9600);
-
   WiFi.begin(SSID, PASS);
-
+  MQTTclient.setServer(mqtt_server, 1885);
+  MQTTclient.setCallback(callback);
   Serial.print("Connecting...");
   while (WiFi.status() != WL_CONNECTED){
     delay(500);
@@ -33,27 +50,76 @@ void setup() {
 }
 
 void loop() {
-  sendGetSensor(1);
-  sendGetMotor(4);
-  delay(3000);
-  sendPostSensor(1,542.5,0.67,132453542);
-  sendPostMotor(2,1.0,123789456);
-  delay(3000000);
+  if (!MQTTclient.connected()) {
+    reconnect();
+  }
+  MQTTclient.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 10000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    MQTTclient.publish("sensor", msg);
+    }
 }
 
-void sendGetSensor(int id){
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!MQTTclient.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (MQTTclient.connect(clientId.c_str(),mqtt_username,mqtt_password)) {
+      Serial.println("connected");
+      MQTTclient.subscribe("sensor", 0);
+      // Once connected, publish an announcement...
+      MQTTclient.publish("info", "hello world",true);
+      // ... and resubscribe
+      MQTTclient.subscribe("sensor", 0);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(MQTTclient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
+/*void sendGetSensor(int id){
   if (WiFi.status() == WL_CONNECTED){
     HTTPClient http;
     http.begin(client, SERVER_IP, SERVER_PORT, "/api/getSensorValueById/" + String(id), true);
     int httpCode = http.GET();
-
     Serial.println("Response code: " + httpCode);
-
     String payload = http.getString();
-
     const size_t capacity = JSON_OBJECT_SIZE(5) + 60;
     DynamicJsonDocument doc(capacity);
-
     DeserializationError error = deserializeJson(doc, payload);
     if (error){
       Serial.print("deserializeJson() failed: ");
@@ -66,7 +132,6 @@ void sendGetSensor(int id){
     float value = doc["value"].as<float>();
     float accuracy = doc["accuracy"].as<float>();
     long timestamp = doc["timestamp"].as<long>();
-
     Serial.println("idsensor_value: " + String(idsensor_value));
     Serial.println("idsensor: " + String(idsensor));
     Serial.println("value: " + String(value));
@@ -80,14 +145,10 @@ void sendGetMotor(int id){
     HTTPClient http;
     http.begin(client, SERVER_IP, SERVER_PORT, "/api/getMotorValueById/" + String(id), true);
     int httpCode = http.GET();
-
     Serial.println("Response code: " + httpCode);
-
     String payload = http.getString();
-
     const size_t capacity = JSON_OBJECT_SIZE(4) + JSON_ARRAY_SIZE(0) + 60;
     DynamicJsonDocument doc(capacity);
-
     DeserializationError error = deserializeJson(doc, payload);
     if (error){
       Serial.print("deserializeJson() failed: ");
@@ -99,7 +160,6 @@ void sendGetMotor(int id){
     float value = doc["value"].as<float>();
     long timestamp = doc["timestamp"].as<long>();
     int idmotor = doc["idmotor"].as<int>();
-
     Serial.println("idmotor_value: " + String(idmotor_value));
     Serial.println("Motor ID: " + String(idmotor));
     Serial.println("Value: " + String(value));
@@ -159,3 +219,4 @@ void sendPostMotor(int idmotor, float value, long timestamp){
     Serial.println("Resultado: " + payload);
   }
 }
+*/
