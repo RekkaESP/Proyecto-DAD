e27a60599ce3f6c699701b954d522cdf3bc187b2
ACCastill0 <61787181+ACCastill0@users.noreply.github.com>
2020-05-28 17:47:53 +0200

    Unidos main.cpp (funcionamiento + api rest y MQTT)

--- sunbot_arduino/sunbot/src/main.cpp
+++ sunbot_arduino/sunbot/src/main.cpp
@@ -5,29 +5,69 @@
 #include <ESP8266HTTPClient.h>
 #include <SoftwareSerial.h>
 #include <PubSubClient.h>
+#include <Servo.h>
 
 char responseBuffer[300];
 WiFiClient client;
-
 String SSID = "FTTH_Palacios_ilCqwk";
 String PASS = "3X76dhNs";
-
 String SERVER_IP = "192.168.100.46";
 int SERVER_PORT = 8090;
 
-///////-MQTT-//////////
-///////////////////////
+//MQTT
 PubSubClient MQTTclient(client);
 unsigned long lastMsg = 0;
 #define MSG_BUFFER_SIZE	(50)
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
-//IPAddress mqtt_server(127, 0, 0, 1);
 const char* mqtt_server = "192.168.100.46";
 const char* mqtt_username = "mqttbroker";
 const char* mqtt_password = "mqttbrokerpass";
-///////////////////////
-///////////////////////
+
+//Motores
+const int motorIzqAdelante = D7;
+const int motorIzqAtras = D8;
+const int motorDerAdelante = D5;
+const int motorDerAtras = D6;
+
+//Sensor ultrasonico
+const int Trigger = D0;
+const int Echo = D1;
+
+//Multiplexor
+const int S0 = D2;
+const int S1 = D3;
+const int sensor = A0;
+
+//servo_motor
+const int servo = D4;
+
+Servo servo_motor;
+boolean haciaDelante = false;
+int distancia = 100;
+int valorSensor = 0;
+int salida = 0;
+int distanciaDer = 0;
+int distanciaIzq = 0;
+int lumIzq = 0;
+int lumDer = 0;
+int distanciaCalc=0;
+int calcLum = 0;
+float diferenciaLum = 0;
+long ultimaLectura = 0;
+long t = 0;
+long d = 0;
+
+void mueveAdelante();
+void mueveAtras();
+void giraIzquierda();
+void giraDerecha();
+void pararMotores();
+void evitaChocar();
+int buscaLuz();
+int calculaDistancia();
+int miraIzquierda();
+int miraDerecha();
 void callback(char* topic, byte* payload, unsigned int length);
 void reconnect();
 void sendGetSensor(int);
@@ -35,8 +75,10 @@ void sendGetMotor(int);
 void sendPostSensor(int,float,float,long);
 void sendPostMotor(int,float,long);
 
+
 void setup() {
   Serial.begin(9600);
+  //WIFI
   WiFi.begin(SSID, PASS);
   MQTTclient.setServer(mqtt_server, 1885);
   MQTTclient.setCallback(callback);
@@ -47,14 +89,49 @@ void setup() {
   }
   Serial.print("Connected, IP address: ");
   Serial.print(WiFi.localIP());
+  //PINES
+  pinMode(Trigger, OUTPUT);
+  pinMode(Echo, INPUT);
+  pinMode(motorDerAdelante, OUTPUT);
+  pinMode(motorIzqAdelante, OUTPUT);
+  pinMode(motorIzqAtras, OUTPUT);
+  pinMode(motorDerAtras, OUTPUT);
+  pinMode(S1,OUTPUT);
+  pinMode(S0,OUTPUT);
+  pinMode(sensor,INPUT);
+  digitalWrite(Trigger, LOW);
+  servo_motor.attach(servo);
+  servo_motor.write(90);
+  delay(75);
 }
 
-void loop() {
+void loop(){
   if (!MQTTclient.connected()) {
     reconnect();
   }
   MQTTclient.loop();
 
+  delay(100);
+  ultimaLectura = ultimaLectura+100;
+  distancia = calculaDistancia();
+  calcLum = buscaLuz();
+  if (distancia <= 20){
+    printf("Objeto a %icm, evitando chocar...\n",distancia);
+    evitaChocar();
+  }else if(calcLum==3){
+    printf("Lectura de luminosidad realizada recientemente, no se leerá.\n");
+    mueveAdelante();
+  }else if(calcLum==0){
+    printf("No hay peligro y luz igual en ambos lados(Luz derecha = %i; Luz izquierda=%i), moviendo hacia delante... [%ld]\n",lumDer,lumIzq,ultimaLectura);
+    mueveAdelante();
+  }else if(calcLum==1){
+    printf("Luz derecha = %i; Luz izquierda=%i, moviendo izquierda...\n",lumDer,lumIzq);
+    giraIzquierda();
+  }else if(calcLum==2){
+    printf("Luz derecha = %i; Luz izquierda=%i, moviendo derecha...\n",lumDer,lumIzq);
+    giraDerecha();
+  }
+  /*
   unsigned long now = millis();
   if (now - lastMsg > 6000) {
     lastMsg = now;
@@ -64,8 +141,10 @@ void loop() {
     Serial.println(msg);
     MQTTclient.publish("sensor", msg);
     }
+    */
 }
 
+//MQTT
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
@@ -95,7 +174,6 @@ void reconnect() {
     // Attempt to connect
     if (MQTTclient.connect(clientId.c_str(),mqtt_username,mqtt_password)) {
       Serial.println("connected");
-      MQTTclient.subscribe("sensor", 0);
       // Once connected, publish an announcement...
       MQTTclient.publish("info", "ESP8266 Conectado",true);
       // ... and resubscribe
@@ -103,15 +181,145 @@ void reconnect() {
     } else {
       Serial.print("failed, rc=");
       Serial.print(MQTTclient.state());
-      Serial.println(" try again in 5 seconds");
+      Serial.println(" trying again in 5 seconds");
       // Wait 5 seconds before retrying
       delay(5000);
     }
   }
 }
 
+//FUNCIONAMIENTO ROBOT
+void evitaChocar(){
+  if(distancia<10){
+    mueveAtras();
+    delay(200);
+  }
+  pararMotores();
+  distanciaDer = miraDerecha();
+  delay(100);
+  distanciaIzq = miraIzquierda();
+  if (distanciaDer >= distanciaIzq){
+    giraIzquierda();
+    delay(200);
+  }else if (distanciaIzq >= distanciaDer){
+    giraDerecha();
+    delay(200);
+  }
+}
 
-/*void sendGetSensor(int id){
+void calculaLuminosidadIzq(){
+  digitalWrite(S1,LOW);
+  digitalWrite(S0,HIGH);
+  lumIzq = analogRead(sensor);
+}
+
+void calculaLuminosidadDer(){
+  digitalWrite(S1,HIGH);
+  digitalWrite(S0,LOW);
+  lumDer = analogRead(sensor);
+}
+
+int calculaHumedad(){
+  int hum = 0;
+  digitalWrite(S1,LOW);
+  digitalWrite(S0,LOW);
+  hum = analogRead(sensor);
+  return hum;
+}
+
+int miraDerecha(){
+  servo_motor.write(30);
+  delay(200);
+  distanciaCalc = calculaDistancia();
+  delay(100);
+  servo_motor.write(90);
+  return distanciaCalc;
+}
+
+int miraIzquierda(){
+  servo_motor.write(150);
+  delay(200);
+  distanciaCalc = calculaDistancia();
+  delay(100);
+  servo_motor.write(90);
+  return distanciaCalc;
+}
+
+void pararMotores(){
+  digitalWrite(motorDerAdelante, LOW);
+  digitalWrite(motorIzqAdelante, LOW);
+  digitalWrite(motorDerAtras, LOW);
+  digitalWrite(motorIzqAtras, LOW);
+}
+
+void mueveAdelante(){
+  digitalWrite(motorIzqAdelante, HIGH);
+  digitalWrite(motorDerAdelante, HIGH);
+  digitalWrite(motorIzqAtras, LOW);
+  digitalWrite(motorDerAtras, LOW);
+}
+
+void mueveAtras(){
+  digitalWrite(motorIzqAtras, HIGH);
+  digitalWrite(motorDerAtras, HIGH);
+  digitalWrite(motorIzqAdelante, LOW);
+  digitalWrite(motorDerAdelante, LOW);
+}
+
+void giraDerecha(){
+  digitalWrite(motorIzqAdelante, LOW);
+  digitalWrite(motorDerAtras, LOW);
+  digitalWrite(motorIzqAtras, HIGH);
+  digitalWrite(motorDerAdelante, HIGH);
+  delay(1000);
+  digitalWrite(motorIzqAdelante, HIGH);
+  digitalWrite(motorDerAdelante, HIGH);
+  digitalWrite(motorIzqAtras, LOW);
+  digitalWrite(motorDerAtras, LOW);
+}
+
+void giraIzquierda(){
+  digitalWrite(motorIzqAtras, HIGH);
+  digitalWrite(motorDerAdelante, HIGH);
+  digitalWrite(motorIzqAdelante, LOW);
+  digitalWrite(motorDerAtras, LOW);
+  delay(1000);
+  digitalWrite(motorIzqAdelante, HIGH);
+  digitalWrite(motorDerAdelante, HIGH);
+  digitalWrite(motorIzqAtras, LOW);
+  digitalWrite(motorDerAtras, LOW);
+}
+
+int calculaDistancia(){
+  servo_motor.write(90);
+  delay(10);
+  digitalWrite(Trigger, HIGH);
+  delayMicroseconds(10);          //Enviamos un pulso de 10us
+  digitalWrite(Trigger, LOW);
+  t = pulseIn(Echo, HIGH); //obtenemos el ancho del pulso
+  d = t/59;             //escalamos el tiempo a una distancia en cm
+  return d;
+}
+
+int buscaLuz() {
+  calculaLuminosidadIzq();
+  calculaLuminosidadDer();
+  diferenciaLum = lumIzq - lumDer;
+  if(ultimaLectura>=500){
+    ultimaLectura = 0;
+    if(diferenciaLum > 100){
+      return 1;
+    }else if(diferenciaLum < -100){
+      return 2;
+    }else{
+      return 0;
+    }
+  }else{
+    return 3;
+  }
+}
+//API REST
+void sendGetSensor(int id){
   if (WiFi.status() == WL_CONNECTED){
     HTTPClient http;
     http.begin(client, SERVER_IP, SERVER_PORT, "/api/getSensorValueById/" + String(id), true);
@@ -219,4 +427,3 @@ void sendPostMotor(int idmotor, float value, long timestamp){
     Serial.println("Resultado: " + payload);
   }
 }
-*/
